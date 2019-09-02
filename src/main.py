179a83fa5045a344a6ec5d9ff0bff64714dc3e8e
codejedi365 <codejedi365@gmail.com>
2019-09-02 14:20:14 -0500

    Docker: Fixed code to line up with adjusted file locations (organization branch)

--- src/main.py
+++ src/main.py
@@ -5,8 +5,6 @@ def main():
 	print("[MAIN] Calling Data_Wrangling_CAP1...")
 	import Data_Wrangling_CAP1
 	print("[MAIN] Calling Exogenous_Variables...")
-	# import 'Exogenous_Variables'
-	import importlib  
-	importlib.import_module("Exogenous_Variables-Copy1")	#because of hyphen
+	import Exogenous_Variables
 
 main()
