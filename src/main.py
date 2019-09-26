0ce24ab0e2165f3c7c5a4c82266837a29cf97635
codejedi365 <codejedi365@gmail.com>
2019-09-26 17:46:39 -0500

    Docker > PrevResults: Load into persistent storage any current results provided during docker build

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@
 
 import getpass
 from os import path
+import shutil
 
 # Main program
 def main():
@@ -10,6 +11,16 @@ def main():
 		import Data_Wrangling_CAP1
 	except SystemExit:
 		pass  # Ignore and continue (accelerated processing due to existing files)
+
+	# Load any previous results if they exist
+	try:
+		prevResults = path.join(path.dirname(path.abspath(__file__)),'data','manipulated_data','allMAE.json')
+		shutil.copyfile(
+			prevResults,
+			path.join(path.expanduser("~"), 'allMAE.json')
+		)
+	except:
+		pass  # Ignore and continue since Exogenous Vars will create the file as default instead of modifying previous results
 
 	print("[MAIN] Calling Exogenous_Variables...")
 	import Exogenous_Variables
